12d4eb1ae4a372c70bd66d81d6a645c8473e465e
ZhangYiWHU <zhang_e2016@outlook.com>
2026-04-09 18:11:31 +0800

    fix: 修改gitignore规则，使cmake/third_party可被追踪

---
+++ cmake/third_party/makeself-fetch.cmake
@@ -0,0 +1,33 @@
+# -----------------------------------------------------------------------------------------------------------
+# Copyright (c) 2025 Tianjin University, Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# -----------------------------------------------------------------------------------------------------------
+
+set(MAKESELF_NAME "makeself")
+set(MAKESELF_PATH ${CANN_3RD_LIB_PATH}/makeself)
+
+# 默认配置的makeself还是不存在则下载
+if (NOT EXISTS "${MAKESELF_PATH}/makeself-header.sh" OR NOT EXISTS "${MAKESELF_PATH}/makeself.sh")
+    set(MAKESELF_URL "https://gitcode.com/cann-src-third-party/makeself/releases/download/release-2.5.0-patch1.0/makeself-release-2.5.0-patch1.tar.gz")
+    message(STATUS "Downloading ${MAKESELF_NAME} from ${MAKESELF_URL}")
+
+    include(FetchContent)
+    FetchContent_Declare(
+        ${MAKESELF_NAME}
+        URL ${MAKESELF_URL}
+        URL_HASH SHA256=bfa730a5763cdb267904a130e02b2e48e464986909c0733ff1c96495f620369a
+        SOURCE_DIR "${MAKESELF_PATH}"  # 直接解压到此目录
+    )
+    FetchContent_MakeAvailable(${MAKESELF_NAME})
+    execute_process(
+        COMMAND chmod 700 "${CMAKE_BINARY_DIR}/makeself/makeself.sh"
+        COMMAND chmod 700 "${CMAKE_BINARY_DIR}/makeself/makeself-header.sh"
+        -E env
+        CMAKE_TLS_VERIFY=0
+        RESULT_VARIABLE CHMOD_RESULT
+        ERROR_VARIABLE CHMOD_ERROR
+    )
+endif()
---
+++ cmake/third_party/protobuf.cmake
@@ -0,0 +1,41 @@
+# -----------------------------------------------------------------------------------------------------------
+# Copyright (c) 2025 Tianjin University, Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# -----------------------------------------------------------------------------------------------------------
+set(_protobuf_url "")
+if(CANN_PKG_SERVER)
+  set(_protobuf_url "${CANN_PKG_SERVER}/libs/protobuf/v25.1.tar.gz")
+endif()
+include(ExternalProject)
+ExternalProject_Add(external_protobuf
+  URL               ${_protobuf_url}
+                    https://gitcode.com/cann-src-third-party/protobuf/releases/download/v25.1/protobuf-25.1.tar.gz
+  DOWNLOAD_DIR      download/protobuf
+  PREFIX            third_party
+  SOURCE_SUBDIR     cmake
+  CMAKE_CACHE_ARGS
+      -DProtobuf_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+      -Dprotobuf_BUILD_TESTS:BOOL=OFF
+      -Dprotobuf_BUILD_EXAMPLES:BOOL=OFF
+      -Dprotobuf_BUILD_SHARED_LIBS:BOOL=OFF
+      -DProtobuf_CXX_COMPILER:STRING=${CMAKE_CXX_COMPILER}
+  INSTALL_COMMAND   ""
+)
+
+ExternalProject_Get_Property(external_protobuf SOURCE_DIR)
+ExternalProject_Get_Property(external_protobuf BINARY_DIR)
+
+set(Protobuf_INCLUDE ${SOURCE_DIR}/src)
+set(Protobuf_PATH ${BINARY_DIR})
+set(Protobuf_PROTOC_EXECUTABLE ${Protobuf_PATH}/protoc)
+
+add_custom_command(
+  OUTPUT ${Protobuf_PROTOC_EXECUTABLE}
+  DEPENDS external_protobuf
+)
+add_custom_target(
+  protoc ALL DEPENDS ${Protobuf_PROTOC_EXECUTABLE}
+)
