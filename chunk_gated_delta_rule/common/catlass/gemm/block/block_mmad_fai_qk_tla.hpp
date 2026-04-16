dd30de84e7407f8816bc0de1963279df97be2e04
ZhangYiWHU <zhang_e2016@outlook.com>
2026-04-16 21:41:12 +0800

    update license

--- chunk_gated_delta_rule/common/catlass/gemm/block/block_mmad_fai_qk_tla.hpp
+++ chunk_gated_delta_rule/common/catlass/gemm/block/block_mmad_fai_qk_tla.hpp
@@ -1,5 +1,5 @@
 /**
-* Copyright (c) 2026 Huawei Technologies Co., Ltd.
+* Copyright (c) 2026 Tianjin University, Ltd.
 * This file is a part of the CANN Open Software.
 * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
 * CANN Open Software License Agreement Version 2.0 (the "License").
--- chunk_gated_delta_rule/common/catlass/gemm/kernel/grouped_matmul_slice_m_per_token_dequant_tla.hpp
+++ chunk_gated_delta_rule/common/catlass/gemm/kernel/grouped_matmul_slice_m_per_token_dequant_tla.hpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2026 Huawei Technologies Co., Ltd.
+* Copyright (c) 2026 Tianjin University, Ltd.
 * This file is a part of the CANN Open Software.
 * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
 * Please refer to the License for details. You may not use this file except in compliance with the License.
