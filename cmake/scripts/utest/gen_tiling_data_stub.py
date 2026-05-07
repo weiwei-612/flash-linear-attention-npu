90b54009b76526f127cb45002fcebed99640e8f2
weiwei-612 <sunww_2022@foxmail.com>
2026-05-07 11:00:19 +0800

    update license
    # message auto-generated for no-merge-commit merge:
    !64 merge update_license into main
    
    update license
    
    Description: update license

--- cmake/scripts/utest/gen_tiling_data_stub.py
+++ cmake/scripts/utest/gen_tiling_data_stub.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # -----------------------------------------------------------------------------------------------------------
-# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# Copyright (c) 2025 Tianjin University, Ltd.
 # This program is free software, you can redistribute it and/or modify it under the terms and conditions of
-# CANN Open Software License Agreement Version 2.0 (the "License").
 # Please refer to the License for details. You may not use this file except in compliance with the License.
 # THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
 # INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
@@ -171,7 +170,7 @@ class Process:
              " * This program is free software, you can redistribute it and/or modify.\n"
              " * Copyright (c) {year} Huawei Technologies Co., Ltd.\n"
              " * This file is a part of the CANN Open Software.\n"
-             " * Licensed under CANN Open Software License Agreement Version 2.0 (the \"License\").\n"
+
              " * Please refer to the License for details. "
              "You may not use this file except in compliance with the License.\n"
              " * THIS SOFTWARE IS PROVIDED ON AN \"AS IS\" BASIS, "
